913250a4846f9023bfa5eafbe2c78cbc75533140
chika-12 <markworship001@gmail.com>
2025-11-06 06:32:42 +0100

    url configuration

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import list_all_books, add_books, LibraryDetailView
+
+urlpatterns = [
+  path("books/", list_all_books, name='list_books'),
+  path("books/add/", add_books, name="add_books"),
+  path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from .models import Author, Librarian, Library, Book
+from django.http import JsonResponse
+from django.views.generic import DeleteView
+
+
+def list_all_books(request):
+  if request.method == "GET":
+    books = Book.objects.all()
+    context = {"books": books}
+    return render(request, "list_book.html", context)
+
+def add_books(request):
+  if request.method == "POST":
+
+    title = request.POST.get('title')
+    author_name = request.POST.get('author')
+    author, created = Author.objects.get_or_create(name=author_name)
+
+    Book.objects.create(title=title, author=author)
+    return redirect('list_books')
+  return render(request, "add_book.html")
+
+class LibraryDetailView(DeleteView):
+  model = Library
+  template_name = 'library_detail.html'
+  context_object_name = 'library'
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['books'] = self.object.books.all()
+    return context
+
+
