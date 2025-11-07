b7b9ed601e88d23b5ec52b8b090f047156a6b7d2
chika-12 <markworship001@gmail.com>
2025-11-07 15:37:36 +0100

    file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,8 @@ from .models import Author, Librarian, Book
 from .models import Library
 from django.http import JsonResponse
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 code = "relationship_app/list_books.html"
 code = "relationship_app/library_detail.html"
@@ -13,7 +15,7 @@ def list_books(request):
   if request.method == "GET":
     books = Book.objects.all()
     context = {"books": books}
-    return render(request, "relationship_app/list_books.html", context)
+    return render(request, "list_book.html", context)
 
 def add_books(request):
   if request.method == "POST":
@@ -28,7 +30,7 @@ def add_books(request):
 
 class LibraryDetailView(DetailView):
   model = Library
-  template_name = 'relationship_app/library_detail.html'
+  template_name = 'library_detail.html'
   context_object_name = 'library'
 
   def get_context_data(self, **kwargs):
@@ -36,4 +38,29 @@ class LibraryDetailView(DetailView):
     context['books'] = self.object.books.all()
     return context
 
+def register_view(request):
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('list_books')
+  else:
+    form = UserCreationForm()
+  return render(request, "register.html", {'form': form})
+
+def login_view(request):
+  if request.method == 'POST':
+    form = AuthenticationForm(data=request.POST)
+    if form.is_valid():
+      user = form.get_user()
+      login(request, user)
+      return redirect('list_books')
+  else:
+    form = AuthenticationForm()
+  return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+  logout(request)
+  return render(request, 'logout.html')
 
