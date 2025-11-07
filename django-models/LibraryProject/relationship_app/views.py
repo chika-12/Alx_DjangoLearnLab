ac6620702b417f1e9618086d258b1b637934751f
chika-12 <markworship001@gmail.com>
2025-11-07 03:42:32 +0100

    add

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,7 @@ from django.http import JsonResponse
 from django.views.generic import DeleteView
 
 code = "relationship_app/list_books.html"
+code = "relationship_app/library_detail.html"
 
 def list_all_books(request):
   if request.method == "GET":
