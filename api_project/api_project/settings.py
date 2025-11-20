3bcbc575c17387214510de613dd87140ee9ce9a6
chika-12 <markworship001@gmail.com>
2025-11-20 18:46:03 +0100

    authorization

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -128,7 +128,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',  # or JWTAuthentication
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
     ],
 }
