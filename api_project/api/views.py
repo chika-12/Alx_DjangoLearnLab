be66bc89276f1a1b80d3ebca6294daea9350926c
chika-12 <markworship001@gmail.com>
2025-11-20 15:41:10 +0100

    created views

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,8 +3,8 @@ from .serializers import BookSerializer
 from .models import Book
 
 # Create your views here.
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
 
-class BookList(ListAPIView):
+class BookList(generics.ListAPIView):
   queryset = Book.objects.all()
   serializer_class = BookSerializer()
